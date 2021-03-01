9a21f90cdac19c398c4f7120749d5fc3049cd7d7
xxnrqzxx <xxnrqzxx@gmail.com>
2021-03-01 16:50:33 +0800

    updated code for Index.js in branch alt1

--- 25thanniv/Index.js
+++ 25thanniv/Index.js
@@ -4,45 +4,88 @@ class Content extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            page: 1
+        };        
+
         this.pageStyle = this.pageStyle.bind(this);
+        this.scrollHandle = this.scrollHandle.bind(this);
         
     }
 
     componentDidMount() {
         const page = document.querySelectorAll(".page");
-        
-        window.addEventListener("wheel", function(event) {
-            if(event.deltaY < 0 ) {
-                alert("scrolled up");
+        page[0].classList.add("active");
+
+        window.addEventListener("wheel", event =()=>  {
+            var a;
+            // alert(event.deltaY);
+            if(event.deltaY < 0) {
+                a = -1;
+                this.scrollHandle(a, page);
             } else {
-                alert("scrolled down");
+                a = 1;
+                this.scrollHandle(a, page);
             }
         });
 
-        this.pageStyle(page);
+        this.pageStyle();
+        
     }
 
     componentWillUnmount() {
         this.pageStyle();
     }
 
-    pageStyle(page) {
+    pageStyle() {
+        const page = document.querySelectorAll(".page");
 
         for(let i=0; i<page.length; i++) {
-            var elements = document.querySelectorAll("[data-pageno]");
+            var elements = document.querySelector("[data-pageno='"+$(page[i]).attr("data-pageno")+"']");
             page[i].style.width = window.innerWidth + "px";
             page[i].style.height = window.innerHeight + "px";
             page[i].style.zIndex = page.length - i;
-            if(elements[i] === $(page[i]).attr("data-pageno")) {
-                alert("test");
+            console.log(elements.className);
+        }
+    }
+
+    scrollHandle(a, page) {
+        var b = this.state.page + a;
+        for(let i = 0; i<page.length; i++) {
+            if(b <= 0) {
+                b = 1;
+            } else if (b >= page.length) {
+                b = page.length;
+            }
+            this.setState(state => ({
+                page: b
+            }));
+            if(page[i].classList.contains("active")) {
+
+                page[i].classList.remove("active");
+                page[i].classList.add("hide");
+
+            }
+
+            if($(page[i]).attr("data-pageno") == this.state.page) {
+
+                $(page[i]).removeClass("hide");
+                setTimeout(function() {
+                    $(page[i]).addClass("active");
+                }, 300);
+                
             }
         }
+        console.log(this.state.page);
+        
     }
 
     render() {
         const page = document.querySelectorAll(".page");
+
+        $(window).resize(this.pageStyle);
+
         
-        window.addEventListener("resize", this.pageStyle(page));
 
         return(
             <div>
